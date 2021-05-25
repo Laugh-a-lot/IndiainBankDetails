b405197b7041d8bbadcd53f3976a04b80d2a93c5
Satyam Kashyap <Laughalot>
2021-05-25 14:36:11 +0530

    commit

--- indianbankdetails/indianbankdetails/wsgi.py
+++ indianbankdetails/indianbankdetails/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indianbankdetails.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indianbankdetails.indianbankdetails.settings')
 
 application = get_wsgi_application()
